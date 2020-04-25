eaa53ded50731d0f0dd0310cbc1faad5e857d86b
Eduardo Aguilar <eaguilar92105@yahoo.com>
2020-04-24 18:23:16 -0700

    created start event listener

---
+++ script.js
@@ -0,0 +1,21 @@
+const questionsEl = document.getElementById('questions');
+const Next = document.getElementsByClassName('btn');
+const startBtn = document.getElementById('start');
+const checkA = document.getElementById('A');
+const checkB = document.getElementById('B');
+const checkC = document.getElementById('C');
+const checkD = document.getElementById('D');
+
+startBtn.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('Started')
+}
+
+function next() {
+
+}
+
+function Answer() {
+
+}
