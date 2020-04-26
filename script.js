f8096674548d2daedc8d2bf89a56ea96e089b29d
Eduardo Aguilar <eaguilar92105@yahoo.com>
2020-04-25 22:52:06 -0700

    Ready for debugging

--- script.js
+++ script.js
@@ -1,21 +1,84 @@
-const questionsEl = document.getElementById('questions');
+const content = document.querySelector('.container');
+const questionEl = document.getElementById('questions');
 const Next = document.getElementsByClassName('btn');
 const startBtn = document.getElementById('start');
-const checkA = document.getElementById('A');
-const checkB = document.getElementById('B');
-const checkC = document.getElementById('C');
-const checkD = document.getElementById('D');
+const scoreEl = document.getElementById('score');
+const A = document.getElementById('A');
+const B = document.getElementById('B');
+const C = document.getElementById('C');
+const D = document.getElementById('D');
+let scoreCount = 0;
+let questions = [
+    {
+        question : "What is the name of Carole's allegedly dead husband?",
+        A : "John Lewis",
+        B : "Don Lewis",
+        C : "Lewis McDonald",
+        D : "John Louis",
+        correct : "B"
+    },{
+        question : "What is Doc Antle's doctarate in?",
+        A : "Tiger Science",
+        B : "Nature Science",
+        C : "Magical Science",
+        D : "Mystical Science",
+        correct : "D"
+    },{
+        question : "HOw many years was Joe Exotic sentenced to prison?",
+        A : "20",
+        B : "30",
+        C : "32",
+        D : "22",
+        correct : "D"
+    },{
+        question : "What did John's Crotch tattoo say?",
+        A : "I am Joe's",
+        B : "Joe Exotic's Private Property",
+        C : "Privately Owned by Joe Exotic",
+        D : "These privates are Joe's Private Property",
+        correct : "C"
+    },
+];
+const lastQuestion = questions.length -1;      
+let runningQuestion = 0;
 
-startBtn.addEventListener('click', startGame)
+startBtn.addEventListener('click', startGame);
 
 function startGame() {
-    console.log('Started')
+    startBtn.setAttribute('style', 'display: none;');
+    renderQuestion();
+    content.setAttribute('style', 'display: block;')
+}
+function renderQuestion() {
+
+    let q = questions[runningQuestion];
+    
+    questionEl.innerHTML = "<p>" + q.question +"</p>";
+    A.innerHTML = q.A;
+    B.innerHTML = q.B;
+    C.innerHTML = q.C;
+    D.innerHTML = q.D;
 }
 
-function next() {
+function checkAnswer(answer) {
+    if( answer == questions[runningQuestion].correct) {
+        // answer is correct
+        scoreCount++;
+    }else {
+        //answer is wrong
+        //decrease timer -10
+    }
+    if(runningQuestion < lastQuestion) {
+        runningQuestion++;
+        renderQuestion();
+    }else{
+        scoreRender();
+    }
+};
+function scoreRender() {
+    scoreEl.setAttribute('style', 'display: block;')
+    content.setAttribute('style', 'display: none;')
 
-}
-
-function Answer() {
-
+    const scorePercent = Math.round(100 * score/questions.length);
+    scoreEl.innerHTML += "<p>Score = "+ scoreCount +"/4 <p>";
 }
